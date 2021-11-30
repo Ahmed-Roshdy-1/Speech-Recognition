9dcaaa2d98b3eae95b32ed890e36aef1586f5163
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-01 00:57:18 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new window.SpeechRecognition();
+
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+  p.innerText = text;
+  texts.appendChild(p);
+  if (e.results[0].isFinal) {
+    p = document.createElement("p");
+  }
+  console.log(text);
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+recognition.start();
